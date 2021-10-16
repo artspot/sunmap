5b1beef4ee5f6c719e52a9a09ce98d85cec7d4ff
artspot <92531900+artspot@users.noreply.github.com>
2021-10-16 10:13:10 +0800

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,6 @@
 export const mountainGrey = "#48454E";
 export const coronaWhite = "#fffefa";
 export const nightBlue = "#223F59";
-export const cloudBlue = "#5B799B";
+export const cloudBlue = "4B0082";
 export const clearYellow = "#FEEFAF";
-export const sunsetAmber = "#F2BE7E";
+export const sunsetAmber = "#FF0000";
